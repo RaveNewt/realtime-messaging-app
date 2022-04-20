2e7eb37697f0cf0d9b0691e1b89022939d4271b6
RaveNewt <ihsanulisra@gmail.com>
2022-04-20 14:52:40 +0700

    fixing bug in channell list component

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -7,7 +7,7 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
 import LogoutIcon from '../assets/logout.png';
 
-const SideBar = () => {
+const SideBar = () => (
     <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
         <div className="icon1__inner">
@@ -20,11 +20,12 @@ const SideBar = () => {
         </div>
     </div>
 </div>
-}
+)
 const ChannelListContainer = () => {
   return (
     <>
         <SideBar />
+        <h1>Hello</h1>
     </>
   );
 }
