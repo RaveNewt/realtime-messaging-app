d754cc799e947e73f32bfc7fe5d0ad50d83f1a31
RaveNewt <ihsanulisra@gmail.com>
2022-04-21 21:35:57 +0700

    Add Message Team and channel. User can choose emoji and editing the message

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
@@ -6,7 +6,10 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
+
+
 const cookies = new Cookies();
 const apiKey = '5kdja7uerxt3';
 const authToken = cookies.get('token');
@@ -24,16 +27,26 @@ if(authToken){
 }
 
 const App = () => {
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
  
   if(!authToken) return <Auth />
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
             <ChannelListContainer
-
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
             />
             <ChannelContainer
-
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                isEditing={isEditing}
+                setIsEditing={setIsEditing}
+                createType={createType}
             />
         </Chat>
     </div>
