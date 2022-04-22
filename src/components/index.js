ad4839ae84999fe52ee1dfe0b90911a0591a8781
Sythe007 <mrtam7777@gmail.com>
2022-04-22 18:43:19 +0700

    container-sidebar-edit-create-search-channel

--- src/components/index.js
+++ src/components/index.js
@@ -5,16 +5,20 @@ from './ChannelListContainer';
 export { default as ChannelSearch }
 from './ChannelSearch';
 export { default as TeamChannelPreview }
-from './TeamChannelPreview'
+from './TeamChannelPreview';
 export { default as TeamChannelList }
-from './TeamChannelList'
+from './TeamChannelList';
 export { default as ChannelInner }
-from './ChannelInner'
+from './ChannelInner';
 export { default as CreateChannel }
-from './CreateChannel'
+from './CreateChannel';
 export { default as EditChannel }
-from './EditChannel'
+from './EditChannel';
 export { default as TeamMessage }
-from './TeamMessage'
+from './TeamMessage';
 export { default as Auth }
-from './Auth'
+from './Auth';
+export { default as UserList }
+from './UserList';
+export { default as ResultsDropdown }
+from './ResultsDropdown';
